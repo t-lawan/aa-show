567888b1204cfe053318e38610bd20b5c3f0dde7
akinsola <thomas.lawanson@gmail.com>
2021-04-19 14:23:08 +0100

    update

--- src/Components/AR/LocationTest.js
+++ src/Components/AR/LocationTest.js
@@ -34,7 +34,8 @@ const LocationTest = () => {
       <a-marker preset="hiro">
         <a-entity
           position="0 0 0"
-          scale="0.05 0.05 0.05"
+          // scale="0.05 0.05 0.05"
+          scale="0.5 0.5 0.5"
           // gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
           gltf-model={Scene}
         ></a-entity>
