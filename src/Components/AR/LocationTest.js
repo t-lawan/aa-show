77806bbc83ce3ab9dffa5cd2bd7b7310bb27cdfa
akinsola <thomas.lawanson@gmail.com>
2021-04-20 14:51:55 +0100

    update

--- src/Components/AR/LocationTest.js
+++ src/Components/AR/LocationTest.js
@@ -17,7 +17,7 @@ const LocationTest = () => {
         embedded
         arjs="sourceType: webcam; videoTexture: false;debugUIEnabled: false;"
       >
-        <a-camera near="0.1" far="50000" fov="80" gps-camera='simulateLatitude: 51.049; simulateLongitude: -0.723' rotation-reader ></a-camera>
+        <a-camera gps-camera='simulateLatitude: 51.049; simulateLongitude: -0.723' rotation-reader ></a-camera>
         <a-text
           value="This content will always face you."
           look-at="[gps-camera]"
