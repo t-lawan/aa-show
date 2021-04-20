ed6bfe109c13529669062e5a79656720afbdc449
akinsola <thomas.lawanson@gmail.com>
2021-04-20 15:18:27 +0100

    update

--- src/Components/AR/LocationTest.js
+++ src/Components/AR/LocationTest.js
@@ -18,13 +18,15 @@ const LocationTest = () => {
         arjs="sourceType: webcam; videoTexture: false;debugUIEnabled: false;"
       >
         <a-camera gps-camera='simulateLatitude: 51.049; simulateLongitude: -0.723' rotation-reader ></a-camera>
+        {/* <a-camera gps-camera rotation-reader ></a-camera> */}
         <a-text
           value="This content will always face you."
           look-at="[gps-camera]"
-          scale="120 120 120"
+          scale="1000 1000 1000"
           gps-entity-place='latitude: 51.0597; longitude: -0.7171'
         ></a-text>
         <a-box material="color: yellow" scale="100 100 100" gps-entity-place='latitude: 51.0597; longitude: -0.7171' position="0 1 0"/>
+        {/* <a-box material="color: yellow" scale="100 100 100" gps-entity-place='latitude: 51.483271; longitude: -0.027009' position="0 1 0"/> */}
 
       </a-scene>
     ) : (
